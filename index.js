163864dd7c681797736327eb5ec006299db3c4cf
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-07 04:34:12 -0600

    block scrolling when nav is open; rotate chevron

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ for(let i = 0; i < expandButtons.length; i++) {
 }
 
 function toggleExpand(i) {
-  if(expandableContent[i].style.display === 'none' || expandableContent[i].style.display === '') {
-    expandButtons[i].style.transform = 'rotate(180deg)'
-    expandableContent[i].style.display = 'flex';
+  // toggle expandable content and:  
+  // if nav expand button, block scrolling when expanded
+  // if chevron expand button, rotate chevron 180deg
+  
+  if(i === 0) {
+    if(expandableContent[i].style.display === 'none' || expandableContent[i].style.display === '') {
+      expandableContent[i].style.display = 'flex';
+      console.log('should not scroll')
+      document.body.style.overflowY = 'hidden';
+    } else {
+      expandableContent[i].style.display = 'none';
+      document.body.style.overflowY = 'auto';
+    }
   } else {
-    expandButtons[i].style.transform = 'rotate(0deg)'
-    expandableContent[i].style.display = 'none';
+    if(expandableContent[i].style.display === 'none' || expandableContent[i].style.display === '') {
+      expandButtons[i].style.transform = 'rotate(180deg)'
+      expandableContent[i].style.display = 'flex';
+    } else {
+      expandButtons[i].style.transform = 'rotate(0deg)'
+      expandableContent[i].style.display = 'none';
+    }
   }
 }
