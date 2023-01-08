a77542e85989e57167fde8de08d3e2477ff3ccba
lil-white-shadow <kru.patel.swe@gmail.com>
2023-01-07 23:16:59 -0600

    reset expandable content display on window resize

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const expandButtons = document.getElementsByClassName('expandButton')
 const expandableContent = document.getElementsByClassName('expandableContent');
 
 const heroCTAButton = document.getElementById('heroCTAButton');
-registerButton = document.getElementById('registerButton');
+const registerButton = document.getElementById('registerButton');
 const registerFormWrapperOuter = document.getElementsByClassName('registerFormWrapperOuter')[0];
 const heroCTA = document.getElementsByClassName('heroCTA')[0];
 const heroContainer = document.getElementsByClassName('heroContainer')[0];
@@ -17,8 +17,9 @@ for(let i = 0; i < expandButtons.length; i++) {
 
 function toggleExpand(i) {
   // toggle expandable content and:  
-  // if nav expand button, block scrolling when expanded
-  // if chevron expand button, rotate chevron 180deg
+  // nav expand button => block scrolling when expanded
+  // footer expand button => toggle only when screenwidth < 621px
+  // if chevron expand button => rotate chevron 180deg
   
   if(i === 0) {
     if(expandableContent[i].style.display === 'none' || expandableContent[i].style.display === '') {
@@ -29,7 +30,10 @@ function toggleExpand(i) {
       expandableContent[i].style.display = 'none';
       document.body.style.overflowY = 'auto';
     }
-  } else {
+  } 
+  
+  if((i > 3 && window.innerWidth < '621') || 
+      (i > 0 && i <= 3)) {
     if(expandableContent[i].style.display === 'none' || expandableContent[i].style.display === '') {
       expandButtons[i].style.transform = 'rotate(180deg)'
       expandableContent[i].style.display = 'flex';
@@ -54,4 +58,33 @@ function displayCTA() {
   setTimeout(() => {
     heroCTA.style.display = 'block';
   }, 300);
+}
+
+window.onresize = resetExpandableContentView;
+
+function resetExpandableContentView() {
+
+  if(window.innerWidth > '620') {
+    for(let i = 4; i < expandableContent.length; i++) {
+      expandableContent[i].style.display = 'flex';
+    }
+  }
+
+  if(window.innerWidth <= '620') {
+    for(let i = 4; i < expandableContent.length; i++) {
+      expandableContent[i].style.display = 'none';
+    }
+  }
+
+  if(window.innerWidth > '425') {
+    for(let i = 1; i < expandableContent.length - 4; i++) {
+      expandableContent[i].style.display = 'flex';
+    }
+  }
+
+  if(window.innerWidth <= '425') {
+    for(let i = 1; i < expandableContent.length - 4; i++) {
+      expandableContent[i].style.display = 'none';
+    }
+  }
 }
